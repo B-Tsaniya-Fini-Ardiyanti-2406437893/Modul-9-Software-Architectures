998787965ac0c79a24daab05353ef824b0bb9702
unknown <fini.ardiyanti@gmail.com>
2026-05-12 09:33:36 +0700

    Simulation slow subscriber

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
         let ten_millis = time::Duration::from_millis(1000);
         let now = time::Instant::now();
         
-        // thread::sleep(ten_millis);
+        thread::sleep(ten_millis);
         
         println!(
             "In Tsaniya Fini Ardiyanti's Computer [2406437893]. Message received: {:?}",
